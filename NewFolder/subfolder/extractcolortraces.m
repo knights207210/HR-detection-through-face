d0416f33e052056f93ab480f006047b40d461acb
Xu Han <hx2013@mail.ustc.edu.cn>
2017-04-12 17:14:01 +0200

    Add files via upload

--- NewFolder/subfolder/extractcolortraces.m
+++ NewFolder/subfolder/extractcolortraces.m
@@ -5,34 +5,43 @@ method = options.getParam('facefeaturemethod');
 if method == 1    %DRMF
     
     [facial_landmarks] = DRMF_facial_landmarks(sVideo);
-    [ROI_boundary_points] = ROI_points(facial_landmarks);
-    [transformation_matrix] = get_transformation_matrix(video);
+    [ROI_boundary_points] = HR_getROI_points(facial_landmarks);
+    [transformation_matrix] = get_transformation_matrix(sVideo);
     % calculate color traces, mean value contain frames from 
     % 1st to last one of the video (not lastFrame set as the parameters)
     show = 0;
-    [mean_value, pathtolandmarks] = mean_value_calculation(ROI_boundary_points,transformation_matrix,video,show);
-    savepath = sprintf('colortraces%d.mat',i);
+    [mean_value] = mean_value_calculation(ROI_boundary_points,transformation_matrix,sVideo,show);
+    savepath1 = sprintf('%s/data/%s/colortraces_DRMF%d.mat',cd,sVideo.name,i);
+    
     % save color traces and their corresponding transformation matrix
-    save(savepath,'transformation_matrix','mean_value');  
-    path_colortraces = savepath;
-    path_landmarks = pathtolandmarks;
+    save(savepath1,'transformation_matrix','mean_value');  
+    path_colortraces = savepath1;
+    savepath2 = sprintf('%s/data/%s/landmarks_DRMF%d.mat',cd,sVideo.name,i);
+    save(savepath2,'facial_landmarks');
+    path_landmarks = savepath2;
+    %path_landmarks = pathtolandmarks;
     
 elseif method == 2      %DILB
+    addpath('/home/hanxu/thesis/NewFolder/subfolder/Package');
     
     VideoFileReader = vision.VideoFileReader(sVideo.path);
-    
-    for k=1:sVideo.lastFrame+3*sVideo.fps
-        frame = step(VideoFileReader);
-        facial_landmarks(k) = dilb(frame);
-        ROI_boundary_points(k) = ROI_points_dilb(facial_landmarks(k));
-        show = 0;
-        mean_value(k) = mean_value_cal_dilb(ROI_boundary_points(k),sVideo,show);   
+    for k=1:sVideo.firstFrame-1
+        [~]=step(VideoFileReader);
     end
-    savepath1 = sprintf('colortraces%d.mat',i);
+    
+    for k=sVideo.firstFrame:sVideo.lastFrame+20*sVideo.fps
+        frame = step(VideoFileReader);
+        frame = im2uint8(frame);
+        facial_landmarks{k} = dlibLandmarks('/home/hanxu/thesis/NewFolder/subfolder/Package/model.dat',frame);
+        ROI_boundary_points{k} = ROI_points_dilb(facial_landmarks{k});
+        show = 0;
+        mean_value(k,:) = average_ROI(frame,ROI_boundary_points{k},show);
+    end
+    savepath1 = sprintf('%s/data/%s/colortraces_DLIB%d.mat',cd,sVideo.name,i);
     save(savepath1,'mean_value');
     path_colortraces = savepath1;
-    savepath2 = sprintf('landmarks%d.mat',i);
-    save(savepath2,'landmarks');
+    savepath2 = sprintf('%s/data/%s/landmarks_DLIB%d.mat',cd,sVideo.name,i);
+    save(savepath2,'facial_landmarks');
     path_landmarks = savepath2;
     
 
---
+++ NewFolder/subfolder/output_videoprocessed.m
@@ -0,0 +1,123 @@
+function [] = output_videoprocessed(sResults,options)
+%output_videoprocessed Summary of this function goes here
+%this function is used to figure and for testing the results; to have a
+%better visulization effect
+
+flag1 = options.getParam('inputtype');
+flag2 = options.getParam('facefeaturemethod');
+
+
+%% calculate and store the image 
+if flag1 == 1   %single video
+path_video = sResults.path;
+path_landmarks = sResults.pathToLandmarks;
+path_colortraces = sResults.pathToColorTraces;
+load(path_landmarks);
+load(path_colortraces);
+
+    if flag2 ==1     %DRMF
+        VideoFileReader = vision.VideoFileReader(path_video);
+        for k=1:sResults.firstFrame-1
+            [~]=step(VideoFileReader);
+        end
+        frame = step(VideoFileReader);
+        points = facial_landmarks.points;
+        pointImage = insertMarker(frame,points,'+','Color','white');
+        imshow(pointImage);
+        savepath = sprintf('%s/data/%s/tmp/%d.jpg',cd,sResults.name,0);
+        saveas(gcf,[savepath]);
+        m=0;
+        for k=sResults.firstFrame+1:sResults.lastFrame
+            m=m+1;
+            frame = step(VideoFileReader);
+            oldpoints = points;
+            newpoints = transformPointsForward(transformation_matrix{k-1},oldpoints);
+            points = newpoints;
+            pointImage = insertMarker(frame,points,'+','Color','white');
+            imshow(pointImage);
+            savepath = sprintf('%s/data/%s/tmp/%d.jpg',cd,sResults.name,m);
+            saveas(gcf,[savepath]);
+        end
+    elseif flag2 == 2    %DILB
+        path_video = sResults.path;
+        path_landmarks = sResults.pathToLandmarks;
+        path_colortraces = sResults.pathToColorTraces;
+        load(path_landmarks);
+        load(path_colortraces);
+        m=0;
+        VideoFileReader = vision.VideoFileReader(path_video);
+        for k =1:sResults.firstFrame-1
+            [~] = step(VideoFileReader);
+        end
+        for k = sResults.firstFrame:sResults.lastFrame
+            m=m+1;
+            frame = step(VideoFileReader);
+            points = facial_landmarks{k};
+            pointImage = insertMarker(frame,points,'+','Color','white');
+            imshow(pointImage);
+            savepath = sprintf('%s/data/%s/tmp/%d.jpg',cd,sResults.name,m-1);
+            saveas(gcf,[savepath]);
+        end
+    end
+%% output video
+ picFormat = 'jpg';
+ syscall = ['ffmpeg -f image2 -i' cd,'/data/',sResults.name,'/%d.',picFormat,' -r ',num2str(sResults.fps),' ',sResults.name,'.avi'];
+ [status,cmdout] = system(syscall,'-echo');
+        
+
+elseif flag1 == 2     %databases
+    for m = 1:length(sResults)
+        path_video = sResults{m}.path;
+        path_landmarks = sResults{m}.pathToLandmarks;
+        path_colortraces = sResults{m}.pathToColorTraces;
+        load(path_landmarks);
+        load(path_colortraces);
+
+     if flag2 ==1     %DRMF
+        VideoFileReader = vision.VideoFileReader(path_video);
+        frame = step(VideoFileReader);
+        points = facial_landmarks.points;
+        pointImage = insertMarker(frame,points,'+','Color','white');
+        imshow(pointImage);
+        path = sprintf('/home/hanxu/thesis/NewFolder/data/tmp/dataset%d',m);
+        mkdir(path);
+        saveas(gcf,['/home/hanxu/thesis/NewFolder/data/tmp/dataset',num2str(m),'/',num2str(0),'.jpg']);
+        
+        %for k=2:sResults{m}.lastFrame
+         for k=2:610
+            frame = step(VideoFileReader);
+            oldpoints = points;
+            newpoints = transformPointsForward(transformation_matrix{k-1},oldpoints);
+            points = newpoints;
+            pointImage = insertMarker(frame,points,'+','Color','white');
+            imshow(pointImage);
+            saveas(gcf,['/home/hanxu/thesis/NewFolder/data/tmp/dataset',num2str(m),'/',num2str(k-1),'.jpg']);
+        end
+     elseif flag2 == 2    %DILB
+        for k = 1:sResults{m}.lastFrame
+            VideoFileReader = vision.VideoFileReader(path_video);
+            frame = step(VideoFileReader);
+            points = landmarks(k);
+            pointImage = insertMarker(frame,points,'+','Color','white');
+            imshow(pointImage);
+            path = sprintf('/home/hanxu/thesis/NewFolder/data/tmp/dataset%d',m);
+            mkdir(path);
+            saveas(gcf,['/home/hanxu/thesis/NewFolder/data/tmp/dataset',num2str(m),'/',num2str(k-1),'.jpg']);
+        end
+     end 
+     picFormat = 'jpg';
+     syscall = ['ffmpeg -f image2 -i /home/hanxu/thesis/NewFolder/data/tmp/dataset',num2str(m),'/%d.',picFormat,' -r ',num2str(sResults{m}.fps),' ',num2str(m),'.avi'];
+    [status,cmdout] = system(syscall,'-echo');
+    end
+
+end
+ 
+
+
+ 
+            
+    
+
+
+end
+
